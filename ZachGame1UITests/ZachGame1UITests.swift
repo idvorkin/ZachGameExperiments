cfe726526dd4891a9a64d7fe6acd4603bfbbb318
Igor Dvorkin <idvorkin@gmail.com>
2015-11-20 21:08:56 -0800

    Initial Commit

---
+++ ZachGame1UITests/ZachGame1UITests.swift
@@ -0,0 +1,36 @@
+//
+//  ZachGame1UITests.swift
+//  ZachGame1UITests
+//
+//  Created by igor dvorkin on 11/20/15.
+//  Copyright © 2015 igor dvorkin. All rights reserved.
+//
+
+import XCTest
+
+class ZachGame1UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
